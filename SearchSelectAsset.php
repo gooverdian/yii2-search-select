ae675fea15bf23a77da7fcf7495b0854e2df3c7b
gooverdian <gooverdian@gmail.com>
2018-02-18 21:48:10 +0300

    Update SearchSelectAsset.php

--- SearchSelectAsset.php
+++ SearchSelectAsset.php
@@ -1,16 +1,31 @@
 <?php
-namespace frontend\assets;
+namespace gooverdian\searchSelect;
 
 use yii\bootstrap\BootstrapAsset;
 use yii\web\AssetBundle;
 
 class SearchSelectAsset extends AssetBundle
 {
-    public $sourcePath = '@webroot/themes/frontend/assets';
-    public $css = ['css/search-select.css'];
-    public $js = ['js/search-select.js'];
+    /**
+     * @inheritdoc
+     */
+    public $jsOptions = ['position' => View::POS_HEAD];
 
+    /**
+     * @inheritdoc
+     */
     public $depends = [
         BootstrapAsset::class,
     ];
+    
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('js', ['js/search-select']);
+        $this->setupAssets('css', ['css/search-select']);
+        parent::init();
+    }
 }
